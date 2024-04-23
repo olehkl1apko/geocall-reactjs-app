9a1f2d42829032a5e2a04e4b937dbdbbef3c336c
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-23 13:03:20 +0300

    FIX: map page

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import { Marker } from "./Marker";
 import { UserInfoCard } from "@/components/UserInfoCard";
@@ -16,24 +18,33 @@ import {
 const GOOGLE_API = import.meta.env.VITE_GOOGLE_API;
 
 const MapPage = () => {
+  const [defaultMapProps, setDefaultMapProps] = useState(null);
   const myLocation = useSelector(selectMyLocation);
   const onlineUsers = useSelector(selectOnlineUsers);
   const cardChosenOption = useSelector(selectCardChosenOption);
+  const navigate = useNavigate();
 
-  const defaultMapProps = {
-    center: {
-      lat: myLocation.lat,
-      lng: myLocation.lng,
-    },
-    zoom: 11,
-  };
+  useEffect(() => {
+    if (myLocation) {
+      const defaultMapProps = {
+        center: {
+          lat: myLocation.lat,
+          lng: myLocation.lng,
+        },
+        zoom: 11,
+      };
+      setDefaultMapProps(defaultMapProps);
+    } else {
+      navigate("/");
+    }
+  }, [myLocation]);
 
   return (
     <div className="map_page_container">
       <GoogleMapReact
         bootstrapURLKeys={{ key: GOOGLE_API }}
-        defaultCenter={defaultMapProps.center}
-        defaultZoom={defaultMapProps.zoom}
+        center={defaultMapProps?.center || { lat: 50.4538739, lng: 30.3538376 }}
+        defaultZoom={defaultMapProps?.zoom || 11}
       >
         {onlineUsers.map((onlineUser) => {
           return <Marker onlineUser={onlineUser} key={onlineUser.socketId} />;
